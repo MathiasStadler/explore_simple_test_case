6c3bf3317b82ad8aa18d67ba84501b6328d4f48f
Mathias Stadler <stadler-mathias@web.de>
2025-08-13 14:54:38 +0200

    restart project

--- build.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    println!("cargo::rerun-if-changed=build.rs");
-    println!("cargo::rerun-if-changed=tests/test_data");
-    println!("cargo::rerun-if-changed=tests/test_data_2");
-}
